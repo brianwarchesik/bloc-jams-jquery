b40f986fae3925efe8b7cd6cfa2815e291ecb55a
Brian Warchesik <warchesikarts@gmail.com>
2019-01-30 09:26:58 -0700

    Checkpoint player bar buttons complete

---
+++ scripts/player-bar.js
@@ -0,0 +1,20 @@
+{
+    $('button#play-pause').on('click', function() {
+        player.playPause();
+        $(this).attr('playState', player.playState);
+    });
+
+    $('button#next').on('click', function() {
+        if (player.playState !== 'playing')
+            {return};
+
+        const CURRENTSONGINDEX = album.songs.indexOf(player.currentlyPlaying);
+        const NEXTSONGINDEX = CURRENTSONGINDEX +1;
+
+        if (NEXTSONGINDEX >= album.songs.length)
+            {return};
+
+        const NEXTSONG = album.songs[NEXTSONGINDEX];
+        player.playPause(NEXTSONG);
+    });
+}
